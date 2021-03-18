fae5da48ed4575fad7bdc402dc23a5e83b69d8e7
joossm <jooss.matthias@gmail.com>
2021-03-18 11:10:29 +0100

    DID CHANGES

--- src/main/java/com/project/cocktailcloud/GameAPIApplication.java
+++ src/main/java/com/project/cocktailcloud/GameAPIApplication.java
@@ -4,11 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // main class to run the web-service
-
-
 @SpringBootApplication
 public class GameAPIApplication {
-
     public static void main(String... args) {
         SpringApplication.run(GameAPIApplication.class, args);
     }
--- src/main/java/com/project/cocktailcloud/GameRepository.java
+++ src/main/java/com/project/cocktailcloud/GameRepository.java
@@ -9,17 +9,17 @@ import java.util.List;
 @Repository
 interface GameRepository extends JpaRepository<Game, Long> {
 
-    //Query for /getGames
+    //Query for /getGame
     @Query
     List<Game> findByMinPlayerLessThanEqualAndMaxPlayerGreaterThanEqualAndGenre1Equals(
             int minPlayer, int maxplayer, String genre1);
 
-    //Query for /getGames
+    //Query for /getGame
     @Query
     List<Game> findByMinPlayerLessThanEqualAndMaxPlayerGreaterThanEqualAndGenre2Equals(
             int minPlayer, int maxplayer, String genre2);
 
-    //Query for /getGames
+    //Query for /getGame
     @Query
     List<Game> findByMinPlayerLessThanEqualAndMaxPlayerGreaterThanEqualAndGenre3Equals(
             int minPlayer, int maxplayer, String genre3);
